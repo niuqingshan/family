d18ee93e9a4f81e27e66f543f725f6f32906de97
torpedoallen <torpedoallen@gmail.com>
2016-05-25 08:59:11 +0000

    add flask

--- family/commands/createapp.py
+++ family/commands/createapp.py
@@ -5,7 +5,8 @@ import subprocess
 
 @click.command()
 @click.argument('project_name')
-def create(project_name):
-    cmd = 'paster create -t falcon %s' % project_name
+@click.option('--framework', '-f', default='falcon', help='framework')
+def create(framework, project_name):
+    cmd = 'paster create -t %s %s' % (framework, project_name)
     p = subprocess.Popen(cmd, shell=True)
     p.communicate()
